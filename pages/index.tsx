52a5a459828de5e069294b3442ae21457407ce87
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2024-03-10 14:26:28 +0300

    Create NextJs app with implementation of main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,48 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+interface ApiFetchProps {
+  message: string;
+  data: {
+    example: string;
+  };
+}
 
-export default IndexPage;
+interface Props {
+  responseData: ApiFetchProps;
+}
+
+const ApiFetch: NextPage<Props> = ({ responseData }) => {
+  return (
+    <div>
+      <h1>Server-Side Rendered Page with API Data</h1>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const headers = {
+    Authorization: `Bearer MyTOKEN###4354`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await fetch("apiAddress", { headers });
+
+    const responseData: ApiFetchProps = await response.json();
+
+    return {
+      props: {
+        responseData,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        responseData: { message: "Error fetching data", data: { example: "" } },
+      },
+    };
+  }
+};
+
+export default ApiFetch;
